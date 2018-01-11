838df9c6fa7fe124177dae8a143af45371c17b63
Reid Linker <reid.linker@gmail.com>
2018-01-10 22:45:52 -0500

    Changing get_image_id function

--- functions.php
+++ functions.php
@@ -43,8 +43,51 @@ require_once(get_template_directory().'/assets/translation/translation.php');
 // require_once(get_template_directory().'/assets/functions/admin.php');
 
 // Retrieves the attachment ID from the file URL
-function get_image_id($image_url) {
-  global $wpdb;
-  $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ));
-  return $attachment[0];
+// @see https://gist.githubusercontent.com/wpscholar/3b00af01863c9dc562e5/raw/8505d35b0bd8887735a0407b704b729eda102782/get-attachment-id.php
+function get_image_id($url) {
+	$attachment_id = 0;
+
+	$dir = wp_upload_dir();
+
+	if ( false !== strpos( $url, $dir['baseurl'] . '/' ) ) {
+		// 		Is URL in uploads directory?
+
+		$file = basename( $url );
+
+		$query_args = array(
+				'post_type'   => 'attachment',
+				'post_status' => 'inherit',
+				'fields'      => 'ids',
+				'meta_query'  => array(
+				array(
+					'value'   => $file,
+					'compare' => 'LIKE',
+					'key'     => '_wp_attachment_metadata',
+				),
+			)
+		);
+
+		$query = new WP_Query( $query_args );
+
+		if ( $query->have_posts() ) {
+
+			foreach ( $query->posts as $post_id ) {
+
+				$meta = wp_get_attachment_metadata( $post_id );
+
+				$original_file       = basename( $meta['file'] );
+				$cropped_image_files = wp_list_pluck( $meta['sizes'], 'file' );
+
+				if ( $original_file === $file || in_array( $file, $cropped_image_files ) ) {
+					$attachment_id = $post_id;
+					break;
+				}
+
+			}
+
+		}
+
+	}
+
+	return $attachment_id;
 }
